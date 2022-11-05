927b76cf394ff198ebc3acbf613af8d91e85bbdb
zhouyuze <82017529+zhouyyyuze@users.noreply.github.com>
2022-11-05 16:43:33 +0800

    Add files via upload

---
+++ COVID-19/main.m
@@ -0,0 +1,18 @@
+clear
+
+load('data0509.mat');
+
+X_train = normalize([modal1, modal2, modal3]);
+train_label = label;
+
+[pvalue,status] = l1_ls_nonneg(X_train, train_label, 0.1);
+pvalue_nonn=abs(pvalue);   
+
+ppp=sort(pvalue_nonn,'descend');
+[ppp, ppp_index]=sort(pvalue_nonn, 'descend');
+
+[data, str] = xlsread('data.xlsx');
+
+str=str(2:end);
+top_20_num = ppp(1:20);
+top_20_str = str(ppp_index(1:20))';
